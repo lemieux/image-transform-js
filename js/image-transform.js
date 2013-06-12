1defef296ae938065eda68d68be89ddb927b98a5
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2013-06-12 08:24:39 -0400

    added the scaling algorithm

--- js/image-transform.js
+++ js/image-transform.js
@@ -11,9 +11,7 @@
 }(function($, Raphael) {
 
     $.fn.imageTransform = function(options) {
-        this.each(function() {
-            init.call(this, options);
-        });
+        init.call(this, options);
         return this;
     };
 
@@ -23,20 +21,23 @@
     }
 
     function Transform(target, opts) {
-        var src = $(target).attr('src');
-
-        if (typeof src === 'undefined' || src === false) {
-            src = '';
-        }
-
-        var initTarget,
+        var initTransform,
+            initTarget,
+            rotate,
             paper,
+            image,
+            realTarget,
+            originalTarget = target,
+            targetIsImage = $(target).is('img'),
             self = this,
             $self = $(self),
             originalWidth = 0,
             originalHeight = 0,
             width = 0,
             height = 0,
+            centerX = 0,
+            centerY = 0,
+            currentAngle = 0,
             container = document.createElement('div'),
             options = $.extend({
                 containerHeight: '100%',
@@ -47,38 +48,114 @@
                 init: $.noop,
                 rotatestart: $.noop,
                 rotatestop: $.noop,
-                src: src,
+                src: $(target).attr('src'),
                 viewBoxHeight: $(target).height(),
-                viewBoxWidth: $(target).width()
+                viewBoxWidth: $(target).width(),
+                rotationAnimation: 500,
+                boxBgColor: '#4a525a'
             }, opts);
 
-        if (typeof src === 'undefined' || src === false) {
-            throw "No src given!";
-        }
 
-        self.target = target;
-
-        $(self.target).hide();
-        $(self.target).after(container);
-
-        $self.on('cropstart', options.cropstart);
-        $self.on('cropstop', options.cropstop);
-        $self.on('destroy', options.destroy);
-        $self.on('init', options.init);
-        $self.on('rotatestart', options.rotatestart);
-        $self.on('rotatestop', options.rotatestop);
+        var degreesToRadian = Math.PI / 180;
 
 
+        initTransform = function() {
 
-        initTarget = function() {
-            paper = Raphael(container, options.containerWidth, options.containerHeight);
-            paper.setViewBox(0, 0, options.viewBoxWidth, options.viewBoxHeight, true);
+
+            if (typeof options.src === 'undefined' || options.src === false) {
+                throw "No src given!";
+            }
+
+            self.target = realTarget;
+            self.img = realTarget[0];
+
+            originalWidth = self.img.naturalWidth;
+            originalHeight = self.img.naturalHeight;
+
+            width = self.img.width;
+            height = self.img.height;
+
+            centerX = width / 2;
+            centerY = height / 2;
+
+            $(self.target).hide();
+            $(self.target).after(container);
+
+            $self.on('cropstart', options.cropstart);
+            $self.on('cropstop', options.cropstop);
+            $self.on('destroy', options.destroy);
+            $self.on('init', options.init);
+            $self.on('rotatestart', options.rotatestart);
+            $self.on('rotatestop', options.rotatestop);
+
+
+
+            initTarget = function() {
+                paper = Raphael(container, options.containerWidth, options.containerHeight);
+                paper.rect(0, 0, options.containerWidth, options.containerHeight).attr({
+                    fill: options.boxBgColor
+                });
+                paper.setViewBox(0, 0, options.viewBoxWidth, options.viewBoxHeight, true);
+                image = paper.image(options.src, 0, 0, width, height);
+            };
+
+            rotate = function(delta) {
+                currentAngle = currentAngle + delta;
+
+                var radians, sint, cost, h1, h2, hh, ww;
+                if (currentAngle <= 90) {
+                    radians = currentAngle * degreesToRadian;
+                } else if (currentAngle <= 180) {
+                    radians = (180 - currentAngle) * degreesToRadian;
+                } else if (currentAngle <= 270) {
+                    radians = (currentAngle - 180) * degreesToRadian;
+                } else {
+                    radians = (360 - currentAngle) * degreesToRadian;
+                }
+
+
+                sint = Math.sin(radians);
+                cost = Math.cos(radians);
+
+                var height = image.attrs.height, width = image.attrs.width;
+
+                h1 = height * height / (width * sint + height * cost);
+                h2 = height * width / (width * cost + height * sint);
+                hh = Math.min(h1, h2);
+                ww = hh * width / height;
+
+                var scale = hh / ww;
+
+                if (options.rotationAnimation !== undefined && typeof options.rotationAnimation === "number") {
+                    image.animate({
+                        transform: "r" + currentAngle + "s" + scale
+                    }, options.rotationAnimation, "<>");
+                } else {
+                    image.transform("r" + currentAngle + "s" + scale);
+                }
+            };
+
+
+            initTarget();
+
+            self.rotate = rotate;
+            $self.trigger('init');
         };
 
+        if (!targetIsImage && options.src) {
+            var targetImg = new Image();
+            targetImg.onLoad(function() {
+                target.after(targetImg);
+                target.hide();
+                realTarget = targetImg;
+                initTransform();
+            });
+            targetImg.src = options.src;
+        } else {
+            realTarget = target;
+            initTransform();
+        }
 
-        initTarget();
-
-        $self.trigger('init');
     }
 
     Transform.version = "0.0.1";
