976c2ab7cf519ff0040c6a59084ac6cf129355ba
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2013-06-12 13:39:19 -0400

    added way to draw crop box

--- js/image-transform.js
+++ js/image-transform.js
@@ -23,6 +23,9 @@
     function Transform(target, opts) {
         var initTransform,
             initTarget,
+            initCrop,
+            drawCropTool,
+            drawHandle,
             rotate,
             paper,
             image,
@@ -50,14 +53,42 @@
                 src: $(target).attr('src'),
                 viewBoxHeight: $(target).height(),
                 viewBoxWidth: $(target).width(),
+                cropHandleColor: '#fff',
+                cropHandleSize: 5,
+                cropBoxColor: '#000',
                 rotationAnimation: 500,
                 boxBgColor: '#4a525a'
             }, opts);
+        /*
+        var p = paper.setFinish(),
+            newTX = 0,
+            newTY = 0,
+            fDx = 0,
+            fDy = 0,
+            tAddX, tAddY, reInitialize = false,
+            start = function() {
 
-        var degreesToRadian = Math.PI / 180;
+            },
+            move = function(dx, dy) {
+                tAddX = dx - fDx, tAddY = dy - fDy, fDx = dx, fDy = dy;
+                if (reInitialize) {
+                    tAddX = 0, fDx = 0, tAddY = 0;
+                    fDy = 0, reInitialize = false;
+                } else {
+                    newTX += tAddX, newTY += tAddY;
+                    p.attr({
+                        transform: "t" + newTX + "," + newTY
+                    });
+                }
 
+            },
+            up = function() {
+                reInitialize = true;
+            };
+        p.drag(move, start, up);
+
+*/
         initTransform = function() {
-
 
             if (typeof options.src === 'undefined' || options.src === false) {
                 throw "No src given!";
@@ -96,6 +127,71 @@
                 image = paper.image(options.src, 0, 0, originalWidth, originalHeight);
             };
 
+            initCrop = function() {
+                var cropButton = paper.rect(10, centerY, 50, 50, 10);
+                cropButton.attr({
+                    'fill': options.boxBgColor,
+                    'fill-opacity': 0.5,
+                    'title': 'Crop'
+                });
+
+                var cropButtonSet = paper.set();
+                cropButtonSet.push(cropButton);
+
+                var cropIcon = drawCropTool(22.5, centerY + 12.5, 25, 25, options.cropHandleSize, options.cropHandleColor, options.cropBoxColor);
+                cropButtonSet.push(cropIcon);
+
+                cropButtonSet.mouseover(function() {
+                    cropButtonSet.attr('fill-opacity', 1);
+                }).mouseout(function() {
+                    cropButtonSet.attr('fill-opacity', 0.5);
+                });
+
+            };
+
+            drawHandle = function(x, y, width, height, color) {
+                return paper.rect(x, y, width, height).attr({
+                    stroke: color,
+                    fill: color,
+                    'fill-opacity': 0.5
+                });
+            };
+
+            drawCropTool = function(x, y, width, height, handleSize, handleColor, boxColor) {
+                var set = paper.set();
+
+                var box = paper.rect(x, y, width, height).attr({
+                    'stroke-dasharray': ['-'],
+                    'stroke': boxColor
+                });
+
+                var handleOffset = handleSize / 2;
+                var centerX = width / 2;
+                var centerY = height / 2;
+
+                var nw = drawHandle(x - handleOffset, y - handleOffset, handleSize, handleSize, handleColor);
+                var n = drawHandle(x + centerX - handleOffset, y - handleOffset, handleSize, handleSize, handleColor);
+                var ne = drawHandle(x + width - handleOffset, y - handleOffset, handleSize, handleSize, handleColor);
+                var w = drawHandle(x - handleOffset, y + centerY - handleOffset, handleSize, handleSize, handleColor);
+                var e = drawHandle(x + width - handleOffset, y + centerY - handleOffset, handleSize, handleSize, handleColor);
+                var sw = drawHandle(x - handleOffset, y + height - handleOffset, handleSize, handleSize, handleColor);
+                var s = drawHandle(x + centerX - handleOffset, y + height - handleOffset, handleSize, handleSize, handleColor);
+                var se = drawHandle(x + width - handleOffset, y + height - handleOffset, handleSize, handleSize, handleColor);
+
+
+                set.push(box);
+                set.push(nw);
+                set.push(n);
+                set.push(ne);
+                set.push(w);
+                set.push(e);
+                set.push(sw);
+                set.push(s);
+                set.push(se);
+
+                return set;
+            };
+
             rotate = function(angle) {
                 $self.trigger('rotatestart');
                 var radians, sint, cost, h1, h2, hh, ww;
@@ -103,13 +199,13 @@
                 angle = angle < 0 ? 360 + angle : angle;
 
                 if (angle <= 90) {
-                    radians = angle * degreesToRadian;
+                    radians = Raphael.rad(angle);
                 } else if (angle <= 180) {
-                    radians = (180 - angle) * degreesToRadian;
+                    radians = Raphael.rad(180 - angle);
                 } else if (angle <= 270) {
-                    radians = (angle - 180) * degreesToRadian;
+                    radians = Raphael.rad(angle - 180);
                 } else {
-                    radians = (360 - angle) * degreesToRadian;
+                    radians = Raphael.rad(360 - angle);
                 }
 
                 sint = Math.sin(radians);
@@ -143,6 +239,7 @@
 
 
             initTarget();
+            initCrop();
 
             realTarget.rotate = self.rotate = rotate;
             $self.trigger('init');
